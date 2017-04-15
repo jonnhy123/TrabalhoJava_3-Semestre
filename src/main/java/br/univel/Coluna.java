007b5761812c2d658db83e636e90202260a98e82
Jhonatan Mattana <jhonatan-mattana@outlook.com>
2017-04-14 21:57:13 -0300

    Implementando @interface tabela

--- src/main/java/br/univel/Coluna.java
+++ src/main/java/br/univel/Coluna.java
@@ -5,7 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.RUNTIME)
+//Informa que a anotação será lida em tempo de execução
+@Retention(RetentionPolicy.RUNTIME)//
+//Pode ser aplicado em um atributo ou propriedade
 @Target(ElementType.FIELD)
 public @interface Coluna {
 	String nome() default "_notset";
