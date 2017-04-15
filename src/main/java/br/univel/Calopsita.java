da4529d8f31dd6feefd036d2607147762b1b1b32
Jhonatan Mattana <jhonatan-mattana@outlook.com>
2017-04-14 22:20:08 -0300

    Ajustando algumas coisas basicas

--- src/main/java/br/univel/Calopsita.java
+++ src/main/java/br/univel/Calopsita.java
@@ -15,20 +15,8 @@ public class Calopsita {
 		this.nome = nome;
 	}
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	 
+	public int getId() {return id;}
+	public void setId(int id) {this.id = id;}
+	public String getNome() {return nome;}
+	public void setNome(String nome) {this.nome = nome;}
 }
