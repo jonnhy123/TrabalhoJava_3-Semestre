b8ea5164d905f6fbc52f8e94dd52468d367b2b1b
Jhonatan Mattana <jhonatan-mattana@outlook.com>
2017-04-18 15:25:44 -0300

    metodo CREATETABLE && INSERTINTO criados!!!

--- src/main/java/reflections/UtilSQL.java
+++ src/main/java/reflections/UtilSQL.java
@@ -1,12 +1,18 @@
 package reflections;
 
 import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import br.univel.Coluna;
 import br.univel.Tabela;
 
 public class UtilSQL {
 
+	private Connection con;
+	
+	//Criar tabela
 	private String CREAE_TABLE (Class<?> clazz){
 		StringBuilder sb = new StringBuilder();
 		//Declaração da tabela
@@ -46,7 +52,107 @@ public class UtilSQL {
 			}else{
 				tipoColuna = "DESCONHECIDO";
 			}
+			
+			if (i > 0) {
+				sb.append(",");
+			}
+			
+			sb.append("\n\t").append(nomeColuna).append(' ').append(tipoColuna);
 		}
+		//Declaração das chaves primárias
+		sb.append(",\nPRIMARY KEY( ");
+		for (int i = 0,achou = 0; i < atributos.length; i++) {
+			Field field = atributos[i];
+			if (field.isAnnotationPresent(Coluna.class)) {
+				Coluna anotacaoColuna = field.getAnnotation(Coluna.class);
+				if (anotacaoColuna.pk()) {
+					if (achou > 0) {
+						sb.append(", ");
+					}
+					if (anotacaoColuna.nome().isEmpty()) {
+						sb.append(field.getName().toUpperCase());
+					}else{
+						sb.append(anotacaoColuna.nome());
+					}
+					achou++;
+				}
+			}
+			sb.append(" )");
+		}
+		sb.append("\n);");
+		return sb.toString();
 	}
 	
+	//Inserir na Tabela
+	private PreparedStatement INSERTINTO(Object obj){
+		Class<? extends Object> clazz = obj.getClass();
+		StringBuilder sb = new StringBuilder();
+		//Declaração da tabela
+		String nomeTabela;
+		if (clazz.isAnnotationPresent(Tabela.class)) {
+			Tabela anotacaoTabela = clazz.getAnnotation(Tabela.class);
+			nomeTabela = anotacaoTabela.value();
+		}else{
+			nomeTabela = clazz.getSimpleName().toUpperCase();
+		}
+		sb.append("INSERT INTO ").append(nomeTabela).append(" (");
+		Field[] atributos = clazz.getDeclaredFields();
+		//Declaração das colunas
+		for (int i = 0; i < atributos.length; i++) {
+			Field field = atributos[i];
+			String nomeColuna;
+			if (field.isAnnotationPresent(Coluna.class)) {
+				Coluna anotacaoColuna = field.getAnnotation(Coluna.class);			
+				if (anotacaoColuna.nome().isEmpty()) {
+					nomeColuna = field.getName().toUpperCase();
+				}else{
+					nomeColuna = anotacaoColuna.nome();
+				}
+			}else{
+				nomeColuna = field.getName().toUpperCase();
+			}
+			if (i > 0) {
+				sb.append(", ");
+			}
+			sb.append(nomeColuna);
+		}
+		sb.append(") VALUES (");
+		
+		for (int i = 0; i < atributos.length; i++) {
+			if (i > 0) {
+				sb.append(", ");
+			}
+			sb.append('?');
+		}
+		sb.append(')');
+		
+		String strSql = sb.toString();
+		System.out.println("SQL GERADO: "+ strSql);
+		
+		PreparedStatement ps = null;
+		
+		try {
+			ps = con.prepareStatement(strSql);
+		
+		for (int i = 0; i < atributos.length; i++) {
+			Field field = atributos[i];
+			//importante não esquecer
+			field.setAccessible(true);
+			if (field.getType().equals(int.class)) {
+				ps.setInt(i + 1, field.getInt(obj));
+			}else if (field.getType().equals(String.class)){
+				ps.setString(i + 1, String.valueOf(field.get(obj)));
+			}else{
+				throw new RuntimeException("Tipo não suportado!!!");
+			}
+		}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		return ps;
+	}
 }
